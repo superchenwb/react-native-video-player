5679249c890c3896274d6017b83034adec93669c
chenwenbin <chen1990wb@163.com>
2020-03-15 02:57:12 +0800

    no message

--- index.js
+++ index.js
@@ -428,7 +428,7 @@ export default class VideoPlayer extends Component {
   }
 
   renderControls() {
-    const { customStyles, Icon } = this.props;
+    const { customStyles, Icon, iconName } = this.props;
     return (
       <View style={[styles.controls, customStyles.controls]}>
         <TouchableOpacity
